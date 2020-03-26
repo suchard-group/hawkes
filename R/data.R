5b5488dfd08bc51ef39a68b230ce67760296cc50
andrewjholbrook <homefolder@Andrews-MacBook-Pro.local>
2020-03-26 12:59:22 -0700

    data done correctly

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Times and spatial locations for gunfire events in Washington D.C. in 2010
+#'
+#' A dataset containing over 5000 gunfire events in Washington D.C. in 2010
+#'
+#' @format A data frame with 5022 rows and 3 variables:
+#' \describe{
+#'   \item{X}{Kilometers from western boundary of Washington D.C. in Eastern direction}
+#'   \item{Y}{Kilometers from southern boundary of Washington D.C. in Northern direction}
+#'   \item{Time}{Minutes from beginning of 2010}
+#' }
+#' @source \url{http://justicetechlab.org/shotspotter-data/}
+"dcData"
